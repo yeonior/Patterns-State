7510e7fcaffed6c359bed212cc21cff2261b00ec
Rus <de.mr44@gmail.com>
2022-02-05 13:14:35 +0500

    Initial Commit

---
+++ Patterns-State/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patterns-State
+//
+//  Created by Ruslan on 05.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
